b1a513c50d2c53a5e55ea53824c62471ff122eb2
root <nitesh.ldd@gmail.com>
2020-06-26 01:19:53 +0530

    INITIAL COMMIT

---
+++ vuefront-starter-kit/clothgram.md
@@ -0,0 +1,10 @@
+## START WITH THIS 
+
+
+
+
+
+
+
+
+kubectl cp  dist wordpress-6f45dc977c-4hmp7:/var/www/html/vuefront/
---
+++ vuefront-starter-kit/nuxt.config.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
+
+export default {
+  mode: 'universal',
+  env: {
+    FEATURED_PRODUCT: process.env.FEATURED_PRODUCT
+  },
+  generate: {
+    concurrency: 5,
+    subFolders: false
+  },
+  head: {
+    title: 'vuefront',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'VueFront' }
+    ],
+    link: [{
+        rel: 'icon',
+        type: 'image/png',
+        href: process.env.BASE_URL+'favicon.ico'
+      }],
+    script: []
+  },
+  loading: { color: '#3B8070' },
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/dotenv',
+    'vuefront-nuxt',
+    'cookie-universal-nuxt'
+  ],
+  pwa: {
+    icon: {},
+    manifest: {
+      name: 'VueFront'
+    },
+  },
+  router: {
+    base: process.env.BASE_URL
+  },
+  build: {
+    babel: {
+      plugins: ['lodash']
+    },
+    extractCSS: true,
+    plugins: [new LodashModuleReplacementPlugin({
+        shorthands: true
+      })]
+  }
+}
